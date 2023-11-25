693195f6eac50eea7bfb19cf749c681cc630d3f4
Shyam Lohar <shyam@apna.co>
2023-11-25 11:55:30 +0530

    chore: setup tailwind and shadcn ui

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  tailwind: true,
+  postcss: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
