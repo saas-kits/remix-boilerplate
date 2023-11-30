783c4a8901280c013ee73898d38f971d39c51feb
Shyam Lohar <shyam@apna.co>
2023-11-30 22:19:35 +0530

    feat: add remix flat routes

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,20 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+import { flatRoutes } from "remix-flat-routes";
+
 export default {
-  ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
+  ignoredRouteFiles: ["**/*"],
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes, {
+      ignoredRouteFiles: [
+        ".*",
+        "**/*.css",
+        "**/*.test.{js,jsx,ts,tsx}",
+        "**/__*.*",
+      ],
+    });
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
