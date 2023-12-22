2ef99c218dd9618b2538c3ba1095e5cfce98d7bc
Shyam Lohar <shyam@apna.co>
2023-12-22 11:33:15 +0530

    feat: add support for sitemap

---
+++ app/lib/server/sitemap/sitemap-utils.server.ts
@@ -0,0 +1,138 @@
+// This is adapted from https://github.com/kentcdodds/kentcdodds.com
+
+import type { ServerBuild } from "@remix-run/server-runtime";
+import type { SEOHandle, SitemapEntry } from "./types.ts";
+import isEqual from "lodash-es/isEqual.js";
+
+console.log({ isEqual });
+
+type Options = {
+  siteUrl: string;
+};
+
+function typedBoolean<T>(
+  value: T
+): value is Exclude<T, "" | 0 | false | null | undefined> {
+  return Boolean(value);
+}
+
+function removeTrailingSlash(s: string) {
+  return s.endsWith("/") ? s.slice(0, -1) : s;
+}
+
+async function getSitemapXml(
+  request: Request,
+  routes: ServerBuild["routes"],
+  options: Options
+) {
+  const { siteUrl } = options;
+
+  function getEntry({
+    route,
+    lastmod,
+    changefreq,
+    priority = 0.7,
+  }: SitemapEntry) {
+    return `
+  <url>
+    <loc>${siteUrl}${route}</loc>
+    ${lastmod ? `<lastmod>${lastmod}</lastmod>` : ""}
+    ${changefreq ? `<changefreq>${changefreq}</changefreq>` : ""}
+    ${typeof priority === "number" ? `<priority>${priority}</priority>` : ""}
+  </url>
+    `.trim();
+  }
+
+  const rawSitemapEntries = (
+    await Promise.all(
+      Object.entries(routes).map(async ([id, { module: mod }]) => {
+        if (id === "root") return;
+
+        const handle = mod.handle as SEOHandle | undefined;
+        if (handle?.getSitemapEntries) {
+          return handle.getSitemapEntries(request);
+        }
+
+        // exclude resource routes from the sitemap
+        // (these are an opt-in via the getSitemapEntries method)
+        if (!("default" in mod)) return;
+
+        const manifestEntry = routes[id];
+
+        console.log({ manifestEntry });
+        if (!manifestEntry) {
+          console.warn(`Could not find a manifest entry for ${id}`);
+          return;
+        }
+        let parentId = manifestEntry.parentId;
+        let parent = parentId ? routes[parentId] : null;
+
+        let path;
+        if (manifestEntry.path) {
+          path = removeTrailingSlash(manifestEntry.path);
+        } else if (manifestEntry.index) {
+          path = "";
+        } else {
+          return;
+        }
+
+        while (parent) {
+          // the root path is '/', so it messes things up if we add another '/'
+          const parentPath = parent.path
+            ? removeTrailingSlash(parent.path)
+            : "";
+
+          if (parent.path) {
+            path = `${parentPath}/${path}`;
+            parentId = parent.parentId;
+            parent = parentId ? routes[parentId] : null;
+          } else {
+            path = `${parentPath}/${path}`;
+            parent = null;
+          }
+        }
+
+        // we can't handle dynamic routes, so if the handle doesn't have a
+        // getSitemapEntries function, we just
+        if (path.includes(":")) return;
+        if (id === "root") return;
+
+        const entry: SitemapEntry = { route: removeTrailingSlash(path) };
+        return entry;
+      })
+    )
+  )
+    .flatMap((z) => z)
+    .filter(typedBoolean);
+
+  const sitemapEntries: Array<SitemapEntry> = [];
+  for (const entry of rawSitemapEntries) {
+    const existingEntryForRoute = sitemapEntries.find(
+      (e) => e.route === entry.route
+    );
+    if (existingEntryForRoute) {
+      if (!isEqual(existingEntryForRoute, entry)) {
+        // if (false) {
+        console.warn(
+          `Duplicate route for ${entry.route} with different sitemap data`,
+          { entry, existingEntryForRoute }
+        );
+      }
+    } else {
+      sitemapEntries.push(entry);
+    }
+  }
+
+  return `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+  >
+    ${sitemapEntries.map((entry) => getEntry(entry)).join("")}
+  </urlset>
+    `.trim();
+}
+
+export { getSitemapXml };
---
+++ app/lib/server/sitemap/types.ts
@@ -0,0 +1,37 @@
+export type SitemapEntry = {
+  route: string;
+  lastmod?: string;
+  changefreq?:
+    | "always"
+    | "hourly"
+    | "daily"
+    | "weekly"
+    | "monthly"
+    | "yearly"
+    | "never";
+  priority?: 0.0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1.0;
+};
+
+export type SEOHandle = {
+  getSitemapEntries?: (
+    request: Request
+  ) =>
+    | Promise<Array<SitemapEntry | null> | null>
+    | Array<SitemapEntry | null>
+    | null;
+};
+
+export type SEOOptions = {
+  siteUrl: string;
+  headers?: HeadersInit;
+};
+
+export type RobotsPolicy = {
+  type: "allow" | "disallow" | "sitemap" | "crawlDelay" | "userAgent";
+  value: string;
+};
+
+export type RobotsConfig = {
+  appendOnDefaultPolicies?: boolean;
+  headers?: HeadersInit;
+};
