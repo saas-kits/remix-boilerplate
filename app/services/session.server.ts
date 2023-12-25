7a48a831eb580fdbf145833b261ac869b89fc918
Keyur Raval <keyur.raval18@gmail.com>
2023-12-25 17:43:33 +0530

    update: use secret key from environment variable

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -9,7 +9,7 @@ export let sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["s3cr3t"], // replace this with an actual secret
+    secrets: [process.env.SESSION_SECRET!], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
