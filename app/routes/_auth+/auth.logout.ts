3a8195dfc5d5f823e7857858f5f51b7ad2dd2fa8
Youssef Adbib <mradbib@gmail.com>
2024-05-13 14:12:08 +0100

    make logout more ux friendly

--- app/routes/_auth+/auth.logout.ts
+++ app/routes/_auth+/auth.logout.ts
@@ -1,7 +1,11 @@
-import type { ActionFunctionArgs } from "@remix-run/node"
+import type { LoaderFunctionArgs,ActionFunctionArgs } from "@remix-run/node"
 
 import { authenticator } from "@/services/auth.server"
 
 export async function action({ request }: ActionFunctionArgs) {
   await authenticator.logout(request, { redirectTo: "/login" })
+}
+export async function loader({ request }: LoaderFunctionArgs) {
+  await authenticator.logout(request, { redirectTo: "/login" })
+  return redirect("/login")
 }
